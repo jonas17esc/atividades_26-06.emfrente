ef60542e07e4e0f76948c4757a4cf887c3d74ffb
duartesilvjonashenry <duartesilvjonashenry@gmail.com>
2026-06-25 17:04:10 +0000

    biblioteca atualizada 25/06

--- biblioteca.js
+++ biblioteca.js
@@ -1,33 +1,58 @@
 const biblioteca = {
     livros: [],
 
-    adicionarLivros(nome, autor, ano, genero) {
+    adicionarLivros(nome, autor, ano, genero, estoque = 1) {
         const livro = {
             nome:nome,
             autor:autor,
             ano:ano,
             genero:genero,
-            disponivel: "sim",
-            alugado: "não",
-            vezesAlugado: 0
+            vezesAlugado: 0,
+            estoque:estoque
         };
 
         this.livros.push(livro);
     },
 
-    mostrarLivros(){
-        for(let i=0; i<this.livros.length; i++){
-            const L= this.livros[i];
-            console.log(L.nome + " - " + L.autor + " - " + L.ano + " - " + L.genero);
+    mostrarLivros() {
+        for (let i = 0; i < this.livros.length; i++) {
+            const l = this.livros[i];
+
+            console.log(
+                `${l.nome} - ${l.autor} - ${l.ano} - ${l.genero} - Estoque: ${l.estoque} - Disponível: ${l.estoque > 0 ? "Sim" : "Não"}`
+            );
         }
+    },
+
+    alugar(nome) {
+        for (let i = 0; i < this.livros.length; i++) {
+            const l = this.livros[i];
+
+            if (l.nome === nome) {
+                if (l.estoque > 0) {
+                    l.estoque--;
+                    l.vezesAlugado++;
+
+                    console.log(`O livro "${nome}" foi alugado com sucesso!`);
+                    return;
+                } else {
+                    console.log(`O livro "${nome}" está sem estoque, por favor escolha outro livro.`);
+                    return;
+                }
+            }
+        }
+
+        console.log(`O livro "${nome}" não existe na biblioteca.`);
     }
 };
+
 
 biblioteca.adicionarLivros(
     "Harry Potter e a Pedra Filosofal",
     "J. K. Rowling",
     1997,
-    "Fantasia"
+    "Fantasia",
+    2
 );
 
 biblioteca.adicionarLivros(
@@ -37,5 +62,8 @@ biblioteca.adicionarLivros(
     "Fantasia"
 );
 
-console.log(biblioteca.livros);
+biblioteca.mostrarLivros();
+
+biblioteca.alugar("Harry Potter e a Pedra Filosofal");
+
 biblioteca.mostrarLivros();
